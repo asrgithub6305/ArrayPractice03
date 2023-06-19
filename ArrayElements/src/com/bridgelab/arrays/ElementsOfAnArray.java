36016fe0964c55b000fcffce248a5fede19bb808
assrrr <asr1997ktq@gmail.com>
2023-06-19 14:33:21 +0530

    Reverse OrderIn An Array

--- ArrayElements/src/com/bridgelab/arrays/ElementsOfAnArray.java
+++ ArrayElements/src/com/bridgelab/arrays/ElementsOfAnArray.java
@@ -3,17 +3,16 @@ package com.bridgelab.arrays;
 import java.util.Arrays;
 
 public class ElementsOfAnArray {
-    //public class OddPositionArray {
+    //public class ReverseOrderArray {
     public static void main(String[] args) {
-        int intArray[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 11};
-        // 0-1, 1-2, 2-3, 3-4, 4-5, 5-6, 6-7, 7-8, 8-9, 9-11 = Index from 0
-        //System.out.println("Array elements at odd position..");
-        for (int i = 0; i < intArray.length; i++) {
-            if (i % 2 == 0) {
+        int arr[] = new int[]{1,2,3,4,5,6};
+        for (int i = arr.length-1; i >= 0; i--) {
+            System.out.print(arr[i] + " ");
+            //ssSystem.out.println("Original array: ");
 
-                System.out.println(intArray[i]);
-            }
         }
     }
 }
+
+
 
