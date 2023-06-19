15951885f5c7bc1fc9cb9e8c0f2d3247cdf35866
assrrr <asr1997ktq@gmail.com>
2023-06-19 13:06:56 +0530

    Array Practice

---
+++ ArrayElements/src/com/bridgelab/arrays/ElementsOfAnArray.java
@@ -0,0 +1,11 @@
+package com.bridgelab.arrays;
+
+public class ElementsOfAnArray {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 28, 14 };
+        System.out.println("Elements of given array are: ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
