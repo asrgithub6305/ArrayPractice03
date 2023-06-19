d4289a6d0fb6af2d7c508d67ec01c82f9bb4973d
assrrr <asr1997ktq@gmail.com>
2023-06-19 14:13:34 +0530

    Duplicate Elements In An Array

--- ArrayElements/src/com/bridgelab/arrays/ElementsOfAnArray.java
+++ ArrayElements/src/com/bridgelab/arrays/ElementsOfAnArray.java
@@ -4,17 +4,13 @@ import java.util.Arrays;
 
 public class ElementsOfAnArray {
     public static void main(String[] args) {
-
-        int[] array = new int[]{7, 3, 8, 1, 6, -23, -5};
-
-        //System.out.print("Elements of original array: ");
-        for (int i = 0; i < array.length; i++) {
-            //System.out.print(array[i] + " ");
+        int[] arr = new int[]{7, 5, 7, 4, 8, 6, 8, 1, 4, 3, 9, 5, 3};
+        System.out.println("Duplicate elements in given array: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j])
+                    System.out.println(arr[j]);
+            }
         }
-        Arrays.sort(array);
-
-        System.out.println();
-
-        System.out.println("Elements of array sorted in ascending order : " + Arrays.toString(array));
     }
 }
