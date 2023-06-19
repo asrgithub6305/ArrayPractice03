0b4b1a5aae95a1fc51e168939f782bbe11f5832b
assrrr <asr1997ktq@gmail.com>
2023-06-19 13:29:49 +0530

    Ascending Order Array

--- ArrayElements/src/com/bridgelab/arrays/ElementsOfAnArray.java
+++ ArrayElements/src/com/bridgelab/arrays/ElementsOfAnArray.java
@@ -1,11 +1,20 @@
 package com.bridgelab.arrays;
 
+import java.util.Arrays;
+
 public class ElementsOfAnArray {
     public static void main(String[] args) {
-        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 28, 14 };
-        System.out.print("Elements of given array are: ");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " ");
+        int[] array = new int[] { 7, 3, 8, 1, 6, -23, -5 };
+
+        //System.out.print("Elements of original array: ");
+        for (int i = 0; i < array.length; i++) {
+            //System.out.print(array[i] + " ");
         }
+        Arrays.sort(array);
+
+        System.out.println();
+
+        System.out.println("Elements of array sorted in ascending order : " + Arrays.toString(array));
+
     }
 }
