3610ebed83c479789ecb44ec002643e5b0f68b91
assrrr <asr1997ktq@gmail.com>
2023-06-19 14:29:50 +0530

    Odd Position In An Array

--- ArrayElements/src/com/bridgelab/arrays/ElementsOfAnArray.java
+++ ArrayElements/src/com/bridgelab/arrays/ElementsOfAnArray.java
@@ -3,11 +3,17 @@ package com.bridgelab.arrays;
 import java.util.Arrays;
 
 public class ElementsOfAnArray {
-    //public class LargeArrayElements {
+    //public class OddPositionArray {
     public static void main(String[] args) {
-        int arr[] = {1, 4, 5, 23, 45, 234, 992, 9233, 99349, 234,};
-        Arrays.sort(arr);
-        System.out.println("Largest number from given array: " + arr[arr.length - 1]);
+        int intArray[] = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 11};
+        // 0-1, 1-2, 2-3, 3-4, 4-5, 5-6, 6-7, 7-8, 8-9, 9-11 = Index from 0
+        //System.out.println("Array elements at odd position..");
+        for (int i = 0; i < intArray.length; i++) {
+            if (i % 2 == 0) {
+
+                System.out.println(intArray[i]);
+            }
+        }
     }
 }
 
