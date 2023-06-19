b5515923081675a7f09385b1951ae4f415dcc106
assrrr <asr1997ktq@gmail.com>
2023-06-19 14:36:37 +0530

    Second Largest Number In An Array

--- ArrayElements/src/com/bridgelab/arrays/ElementsOfAnArray.java
+++ ArrayElements/src/com/bridgelab/arrays/ElementsOfAnArray.java
@@ -3,16 +3,36 @@ package com.bridgelab.arrays;
 import java.util.Arrays;
 
 public class ElementsOfAnArray {
-    //public class ReverseOrderArray {
-    public static void main(String[] args) {
-        int arr[] = new int[]{1,2,3,4,5,6};
-        for (int i = arr.length-1; i >= 0; i--) {
-            System.out.print(arr[i] + " ");
-            //ssSystem.out.println("Original array: ");
-
+    //public class SecondLargestNumberInAnArray {
+    static void print2largest(int arr[], int arr_size)
+    {
+        int i, first, second;
+        if (arr_size < 2) {
+            System.out.println(" Invalid Input ");
+            return;
         }
+        int largest = second = Integer.MIN_VALUE;
+        for(i = 0; i < arr_size; i++)
+        {
+            largest = Math.max(largest, arr[i]);
+        }
+
+        for(i = 0; i < arr_size; i++)
+        {
+            if (arr[i] != largest)
+                second = Math.max(second, arr[i]);
+        }
+        if (second == Integer.MIN_VALUE)
+            System.out.println("There is no second " + "largest element");
+        else
+            System.out.printf("The second largest " + "element is %d", second);
+    }
+    public static void main(String[] args)
+    {
+        int arr[] = { 12, 35, 1, 10, 34, 1 };
+        int n = arr.length;
+        print2largest(arr, n);
     }
 }
-
 
 
